fb72182f02a94ebdc414162a86a9ab906c7a27ec
HenryC-3 <2324375439@qq.com>
2022-10-24 19:30:11 +0800

    feat: create minified output via rollup-plugin-terser

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,20 @@
+import { terser } from "rollup-plugin-terser";
+
 export default {
     input: "./index.js",
     watch: {
         include: "index.js",
     },
-    output: {
-        file: "bundle.js",
-        format: "cjs",
-    },
+    output: [
+        {
+            file: "bundle.js",
+            format: "cjs",
+        },
+        {
+            file: "bundle.min.js",
+            format: "iife",
+            name: "version",
+            plugins: [terser()],
+        },
+    ],
 };
